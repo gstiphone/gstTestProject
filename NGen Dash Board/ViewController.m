7af23c86dcf8a5d325fc98a81d4512cd3662e2c4
GST <iphonedeveloper@gstindia.org>
2016-02-24 12:30:20 +0530

    change color

--- NGen Dash Board/ViewController.m
+++ NGen Dash Board/ViewController.m
@@ -16,8 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor blackColor];
     //lets take a look///////////..........
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
